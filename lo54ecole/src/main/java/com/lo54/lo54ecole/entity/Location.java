0ad5dd72e02259690cfadad1245fac77e5b71261
trivetroff <tristan@vetroff.fr>
2019-10-15 15:33:04 +0200

    create Location, CourseSessions and Course classes without DAO

---
+++ lo54ecole/src/main/java/com/lo54/lo54ecole/entity/Location.java
@@ -0,0 +1,39 @@
+package com.lo54.lo54ecole.entity;
+
+import java.io.Serializable;
+
+public class Location implements Serializable {
+
+    private long location_id;
+    private String city;
+
+    public Location() {
+
+    }
+
+    public Location(long location_id, String city) {
+        this.location_id = location_id;
+        this.city = city;
+    }
+
+    public long getLocationId() {
+        return location_id;
+    }
+
+    public void setLocatonId(long location_id) {
+        this.location_id = location_id;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" + "location_id=" + location_id + ", city=" + city + '}';
+    }
+}
