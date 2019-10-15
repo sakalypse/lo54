d327006aa7f59bc04f0530d8c4042b9012ad0094
trivetroff <tristan@vetroff.fr>
2019-10-15 14:41:25 +0200

    add client serializer without DAO

--- lo54ecole/src/main/java/com/lo54/lo54ecole/entity/Client.java
+++ lo54ecole/src/main/java/com/lo54/lo54ecole/entity/Client.java
@@ -4,46 +4,87 @@ import java.io.Serializable;
 
 public class Client implements Serializable {
 
-    private String title;
-    private String genre;
-    private Integer nbExemplaires;
+    private String client_id;
+    private String lastname;
+    private String firstname;
+    private String addresse;
+    private String phone;
+    private String email;
+    private String course_session_id;
 
     public Client() {
     
     }
 
-    public Client(String title, String genre, Integer nbExemplaires) {
-        this.title = title;
-        this.genre = genre;
-        this.nbExemplaires = nbExemplaires;
+    public Client(String client_id, String lastname, String firstname, 
+                  String addresse, String phone, String email, String course_session_id) {
+        this.client_id = client_id;
+        this.lastname = lastname;
+        this.firstname = firstname;
+        this.addresse = addresse;
+        this.phone = phone;
+        this.email = email;
+        this.course_session_id = course_session_id;
     }
 
-    public String getTitle() {
-        return title;
+    public String getClientId() {
+        return client_id;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
+    public void setClientId(String client_id) {
+        this.client_id = client_id;
     }
 
-    public String getGenre() {
-        return genre;
+    public String getLastname() {
+        return lastname;
     }
 
-    public void setGenre(String genre) {
-        this.genre = genre;
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
     }
 
-    public Integer getNbExemplaires() {
-        return nbExemplaires;
+    public String getFirstname() {
+        return firstname;
     }
 
-    public void setNbExemplaires(Integer nbExemplaires) {
-        this.nbExemplaires = nbExemplaires;
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getAddresse() {
+        return addresse;
+    }
+
+    public void setAddresse(String addresse) {
+        this.addresse = addresse;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCourseSessionId() {
+        return course_session_id;
+    }
+
+    public void setCourseSessionId(String course_session_id) {
+        this.course_session_id = course_session_id;
     }
 
     @Override
     public String toString() {
-        return "Film{" + "title=" + title + ", genre=" + genre + ", nbExemplaires=" + nbExemplaires + '}';
+        return "Client{" + "client_id=" + client_id + ", lastname=" + lastname + ", firstname=" + firstname + ", firstname=" + firstname + ", addresse=" + addresse + ", phone=" + phone + ", email=" + email + ", course_session_id=" + course_session_id + '}';
     }
 }
--- lo54ecole/src/main/java/com/lo54/lo54ecole/repository/ClientDAO.java
+++ lo54ecole/src/main/java/com/lo54/lo54ecole/repository/ClientDAO.java
@@ -1,10 +1,21 @@
 package com.lo54.lo54ecole.repository;
 
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
 import com.lo54.lo54ecole.entity.Client;
 
 public class ClientDAO {
 
-    public void save(Client c) {
-        //save it
+    private Session currentSession;
+     
+    private Transaction currentTransaction;
+ 
+    public ClientDAO() {
     }
 }
