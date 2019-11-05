639f773e19a935c3cba5bad054b3f9bba3147913
Kevin Limacher <kevin.limacher@gmail.com>
2019-11-05 15:18:44 +0100

    add servlet catalog

--- lo54ecole/src/main/java/com/lo54/lo54ecole/service/CourseSessionService.java
+++ lo54ecole/src/main/java/com/lo54/lo54ecole/service/CourseSessionService.java
@@ -3,10 +3,33 @@ package com.lo54.lo54ecole.service;
 import com.lo54.lo54ecole.entity.CourseSession;
 import com.lo54.lo54ecole.repository.CourseSessionDAO;
 
+import java.util.List;
+
 public class CourseSessionService {
     public CourseSession getCourseSessionById(int id){
         CourseSessionDAO csDao = new CourseSessionDAO();
         return csDao.GetById(id);
+    }
+
+    //TODO :
+    public List<CourseSession> getCourseSessionByTitle(String title){
+        CourseSessionDAO csDao = new CourseSessionDAO();
+        return null;
+    }
+
+    public List<CourseSession> getCourseSessionByDate(String date){
+        CourseSessionDAO csDao = new CourseSessionDAO();
+        return null;
+    }
+
+    public List<CourseSession> getCourseSessionByLocation(String location){
+        CourseSessionDAO csDao = new CourseSessionDAO();
+        return null;
+    }
+
+    public List<CourseSession> getAll(){
+        CourseSessionDAO csDao = new CourseSessionDAO();
+        return csDao.getAll();
     }
 
     public void saveCourseSession(CourseSession cs){
---
+++ lo54ecole/src/main/java/com/lo54/lo54ecole/servlet/CatalogServlet.java
@@ -0,0 +1,58 @@
+package com.lo54.lo54ecole.servlet;
+
+import com.lo54.lo54ecole.entity.CourseSession;
+import com.lo54.lo54ecole.service.CourseSessionService;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.*;
+import java.util.*;
+
+@WebServlet( name="CatalogServlet", urlPatterns = {"/", "/index", "/catalog"} )
+public class CatalogServlet extends HttpServlet {
+
+    CourseSessionService courseSessionService = new CourseSessionService();
+
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        String action = request.getParameter("searchAction");
+
+        List<CourseSession> listCourseSession = null;
+
+        if (action!=null){
+            switch (action) {
+                case "searchByTitle":
+                    listCourseSession = courseSessionService.
+                            getCourseSessionByTitle(request.getParameter("searchValue"));
+                    break;
+                case "searchByDate":
+                    listCourseSession = courseSessionService.
+                            getCourseSessionByDate(request.getParameter("searchValue"));
+                    break;
+                case "searchByLocation":
+                    listCourseSession = courseSessionService.
+                            getCourseSessionByLocation(request.getParameter("searchValue"));
+                    break;
+            }
+        }else{
+            listCourseSession = courseSessionService.getAll();
+        }
+
+
+        PrintWriter out = response.getWriter();
+        out.println("<HTML>");
+        out.println("<HEAD>");
+        out.println("<TITLE>Index</TITLE>");
+        out.println("</HEAD>");
+        out.println("<BODY>");
+        out.println("Hello World");
+        for(CourseSession cs : listCourseSession){
+            out.println(cs.toString()+"<br/>");
+        }
+        out.println("</BODY>");
+        out.println("</HTML>");
+    }
+}
